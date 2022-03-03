3fb2da8c8e9c62b950eab112654c30f116fec2d5
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-03 11:23:54 +0530

    created to delete first value in linked list

--- src/linkedlist/LL.java
+++ src/linkedlist/LL.java
@@ -1,65 +1,85 @@
 package linkedlist;
+	
+ /* 
+  * Steps
+ * ==================================
+ * 1.Starting of node head;
+ * 2.creating variable
+ * 3.creating constructor
+ * 4.Created method to add values to the first value
+ * 5.Created method to delete First value
+ * 6.Created method to Print the LinkedList
+ * ====================================
+ */
 
-public class LL {
-	Node head; // Start head
+	public class LL {
+		/*
+		 * 1.Starting of node head
+		 */
 
-	class Node {
-		int data;
-		Node next;
+		Node head;
 
-		Node(int data) {
-			this.data = data;
-			this.next = null;
+		/*
+		 * 2.creating variable
+		 */
+		class Node {
+			int data;
+			Node next;
+
+			/*
+			 * 3. creating constructor
+			 */
+			Node(int data) {
+				this.data = data;
+				this.next = null;
+			}
 		}
-	}
 
-	// add to first value
-	public void addFirst(int data) {
-		Node newNode = new Node(data);
+		/*
+		 * 4.Created method to add values to the first value
+		 */
+		public void addFirst(int data) {
+			Node newNode = new Node(data);
 
-		if (head == null) {
+			if (head == null) {
+				head = newNode;
+				return;
+			}
+			newNode.next = head;
 			head = newNode;
-			return;
-		}
-		newNode.next = head;
-		head = newNode;
-	}
-
-	public void deleteFirst() { // to delete first value
-
-		if (head == null) {
-			System.out.println("the list is empty");
-			return;
-		}
-		head = head.next;
-	}
-
-	public void printList() { // to print output
-
-		if (head == null) {
-			System.out.println("list is empty");
-			return;
-		}
-		Node currNode = head;
-		while (currNode != null)
-
-		{
-			System.out.print(currNode.data + "->");
-			currNode = currNode.next;
 		}
 
-		System.out.println("Null");
+		/*
+		 * 5.Created method to delete First value
+		 */
+		public void deleteFirst() {
+
+			if (head == null) {
+				System.out.println("the list is empty");
+				return;
+			}
+			System.out.println("deleted first value");
+			head = head.next;
+
+		}
+
+		/*
+		 * 6.Created method to Print the LinkedList
+		 */
+		public void printList() { // to print output
+
+			if (head == null) {
+				System.out.println("list is empty");
+				return;
+			}
+			Node currNode = head;
+			while (currNode != null)
+
+			{
+				System.out.print(currNode.data + "->");
+				currNode = currNode.next;
+			}
+
+			System.out.println("Null");
+		}
 	}
-
-	public static void main(String[] args) {
-		LL list = new LL();
-		list.addFirst(70);
-		list.addFirst(30);
-		list.addFirst(56);// addind 56 to linked list
-		list.printList();// printing list
-
-		list.deleteFirst(); // deleting 1st value
-		list.printList();
-
-	}
-}
---
+++ src/linkedlist/Main.java
@@ -0,0 +1,47 @@
+package linkedlist;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * PROCEDURE
+		 * ================================== 
+		 * 1.creating an object 
+		 * 2.adding values to the first place as 56->30->70 
+		 * 3.Printing linkedList 
+		 * 4.deleting first value in linked list 
+		 * 5. Printing the updated linkedList
+		 * ==================================
+		 */
+
+		/*
+		 * 1.creating an object
+		 */
+		LL list = new LL();
+
+		/*
+		 * 2.adding values to the first place as 56->30->70
+		 */
+		list.addFirst(70);
+		list.addFirst(30);
+		list.addFirst(56);
+
+		/*
+		 * 3.Printing linkedList
+		 */
+		list.printList();
+
+		/*
+		 * 4.deleting first value in linked list
+		 */
+		list.deleteFirst();
+
+		/*
+		 * 5. Printing the updated linkedList
+		 */
+		list.printList();
+
+	}
+
+}
