da61e1906b762a265a21a08e208d312f30b7a51e
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-03 11:49:31 +0530

    added simple linked list

--- src/linkedlist/LL.java
+++ src/linkedlist/LL.java
@@ -1,25 +1,37 @@
 package linkedlist;
+/*
+ * Steps
+ * ===============================
+ * 1.creating variables for linklist
+ * 2.creating contstructor
+ * 3.to strings to print the linklist
+ * ================================
+ */
 
 public class LL {
+	/*
+	 * 1.creating variables for linklist
+	 */
 	public int a;
 	public int b;
 	public int c;
 	public LL next;
 
-	public LL(int a, int b, int c, LL next) { // constructor
+	/*
+	 * 2.creating contstructor
+	 */
+	public LL(int a, int b, int c, LL next) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		this.next = next;
 	}
 
-	public String toString() { // print value
+	/*
+	 * 3.to strings to print the linklist
+	 */
+	public String toString() {
 		return a + "->" + b + "->" + c;
 
-	}
-
-	public static void main(String[] args) {
-		LL front = new LL(56, 30, 70, null);// creating object front
-		System.out.println(front);// calling object
 	}
 }
---
+++ src/linkedlist/Main.java
@@ -0,0 +1,26 @@
+package linkedlist;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	    /*
+	     * PROCEDURE
+	     * ====================
+	     * 1.Creating an object
+	     * 2.Printing the Object
+	     * =====================
+	     */
+		
+		/*
+		 * 1.Creating an object
+		 */
+		LL front = new LL(56, 30, 70, null);
+
+		/*
+		 * 2.Printing the Object
+		 */
+		System.out.println(front);
+	}
+
+}
